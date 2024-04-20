d97620de6364b081c7beac5ae19a84cc86c5e90b
Ridhwiman <rikroy2399@gmail.com>
2024-04-20 14:45:46 +0530

    netflix clone ist commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./App.css";
+import HomeScreen from "./screens/HomeScreen";
+import LoginScreen from "./screens/LoginScreen";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+function App() {
+  const user = null;
+  return (
+    <div className="app">
+      <Router>
+        {!user ? (
+          <LoginScreen />
+        ) : (
+          <Routes>
+            <Route exact path="/" element={<HomeScreen />} />
+          </Routes>
+        )}
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBLSNx95DanBT5AJ_hCt2RNe0BuW0cOBg0",
+    authDomain: "netflix-clone-f5cc3.firebaseapp.com",
+    projectId: "netflix-clone-f5cc3",
+    storageBucket: "netflix-clone-f5cc3.appspot.com",
+    messagingSenderId: "564017760490",
+    appId: "1:564017760490:web:b942803a1ace510aa8ef93"
+  };
